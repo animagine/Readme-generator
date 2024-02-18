eaa77d744b19221f4d5c32f8a3bfe186e4a0d5cc
animagine <animagine@live.com>
2024-02-18 12:17:50 +0000

    added user questions

--- index.js
+++ index.js
@@ -6,7 +6,59 @@ const generateMarkdown = require("./utils/generateMarkdown");
 // array of questions for user
 const questions = [
 
-];
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+      },
+      {
+        type: "input",
+        message: "Describe your project",
+        name: "description",
+      },
+      {
+        type: "input",
+        message:
+          "Describe the steps required to install your project for the Installation section.",
+        name: "Installation",
+      },
+      {
+        type: "input",
+        message: "Provide instructions on how to use this project",
+        name: "Usage",
+      },
+      {
+        type: "list",
+        message: "Choose a license for your project.",
+        name: "license",
+        choices: [
+          "GNU AGPLv3",
+          "GNU GPLv3",
+          "GNU LGPLv3",
+          "Mozilla Public License 2.0",
+          "Apache License 2.0",
+          "MIT License",
+          "Boost Software License 1.0",
+          "The Unlicense",
+        ],
+      },
+      {
+        type: "input",
+        message:
+          "Provide any tests written for this application and how to run them",
+        name: "Tests",
+      },
+      {
+        type: "input",
+        message: "What is your Github username?",
+        name: "username",
+      },
+      {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+      },
+    ];
 
 // function to write README file
 function writeToFile(fileName, data) {
